d75af692745ff5450b6d4d0c045f8b1a344d8f2d
Asma <asmi.mha+1@gmail.com>
2023-02-24 06:48:30 +1100

    Added webpack support for css, svg and files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
   module: {
     rules: [
       {
@@ -17,6 +23,17 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+          test: /\.(eot|otf|ttf|woff|woff2)$/,
+          loader: require.resolve("file-loader"),
+          options: {
+            name: "static/media/[name].[hash:8].[ext]"
+          }
       }
     ],
   },
@@ -24,6 +41,11 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
   },
   mode: 'development',
+  devServer: {
+    compress: true,
+    open: true,
+    port: 9000,
+  },
   performance: { 
     hints: false,
     maxEntrypointSize: 512000,

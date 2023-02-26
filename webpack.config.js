2108bd1fc42f64d15de6adeb4c767002a8bde75c
Asma <asmi.mha+1@gmail.com>
2023-02-27 08:53:59 +1100

    Updated Readme and webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, './build'),
+    filename: 'index.bundle.js'
+  },
+  mode: 'development',
+  devServer: {
+    compress: true,
+    open: true,
+    contentBase: './build',
+    port: 9000,
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -39,12 +47,6 @@ module.exports = {
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js'],
-  },
-  mode: 'development',
-  devServer: {
-    compress: true,
-    open: true,
-    port: 9000,
   },
   performance: { 
     hints: false,
